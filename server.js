f9db9c2d92b485cbbb15fcd57a39ce4cb50249f1
Chris Kramer <ChrisKramer15@outlook.com>
2020-08-09 21:06:12 -0500

    case sensitive heroku?

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/apiroutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const htmlRoutes = require("./routes/htmlroutes");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
